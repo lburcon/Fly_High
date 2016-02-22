f260eb8789bff1fe007dc08f9060e0da201cee8c
StoltmanJan <stoltmanjan@gmail.com>
2016-02-22 19:56:39 +0100

    Data parse upgrade

--- FlyHighConference/app/src/main/java/com/example/yggdralisk/flyhighconference/SplashScren.java
+++ FlyHighConference/app/src/main/java/com/example/yggdralisk/flyhighconference/SplashScren.java
@@ -13,6 +13,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import io.realm.Realm;
 import okhttp3.OkHttpClient;
@@ -23,14 +24,14 @@ public class SplashScren extends Activity {
 
     private final int SPLASH_DISPLAY_LENGTH = 10000;
     private final String DATA_HOST_URL = "http://flyhigh.pwr.edu.pl/api/";
-    private final String[] DATA_BASIC = {"organisers","partners","places","presentations","speakers","users"};
+    private final String[] DATA_BASIC = {"organisers", "partners", "places", "presentations", "speakers", "users"};
     private TextView splashMessege;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_scren);
-        splashMessege = (TextView)findViewById(R.id.splash_messege);
+        splashMessege = (TextView) findViewById(R.id.splash_messege);
         new getDataTask().execute();
     }
 
@@ -41,6 +42,14 @@ public class SplashScren extends Activity {
 
     private class getDataTask extends AsyncTask<Void, Void, Void> {
         JSONObject js = new JSONObject();
+        JSONArray organisersArray = new JSONArray();
+        JSONArray partnersArray = new JSONArray();
+        JSONArray placesArray = new JSONArray();
+        JSONArray presentationsArray = new JSONArray();
+        JSONArray speakersArray = new JSONArray();
+        JSONArray usersArray = new JSONArray();
+
+        Boolean connectionFailed = false;
 
         @Override
         protected void onPreExecute() {
@@ -52,50 +61,37 @@ public class SplashScren extends Activity {
 
             try {
                 js = new JSONObject(getData("import"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
 
-            return null;
+                organisersArray = new JSONArray(js.get(DATA_BASIC[0]).toString());
+                partnersArray = new JSONArray(js.get(DATA_BASIC[1]).toString());
+                placesArray = new JSONArray(js.get(DATA_BASIC[2]).toString());
+                presentationsArray = new JSONArray(js.get(DATA_BASIC[3]).toString());
+                speakersArray = new JSONArray(js.get(DATA_BASIC[4]).toString());
+                usersArray = new JSONArray(js.get(DATA_BASIC[5]).toString());
+            } catch (JSONException e) {//TODO:Zapytać się Wojtka o catch in trycatch w androidzie
+                try {
+                    organisersArray = new JSONArray(getData(DATA_BASIC[0]));
+                    partnersArray = new JSONArray(getData(DATA_BASIC[1]));
+                    placesArray = new JSONArray(getData(DATA_BASIC[2]));
+                    presentationsArray = new JSONArray(getData(DATA_BASIC[3]));
+                    speakersArray = new JSONArray(getData(DATA_BASIC[4]));
+                    usersArray = new JSONArray(getData(DATA_BASIC[5]));
+                } catch (JSONException e1) { //TODO:Dopracować co zrobić w razie braku połączenia
+                    connectionFailed = true;
+                }
+
+            } return null;
         }
+
 
         @Override
         protected void onPostExecute(Void result) {
-            JSONArray organisersArray = new JSONArray();
-            JSONArray partnersArray = new JSONArray();
-            JSONArray placesArray = new JSONArray();
-            JSONArray presentationsArray = new JSONArray();
-            JSONArray speakersArray = new JSONArray();
-            JSONArray usersArray = new JSONArray();
-
-            try {
-                 organisersArray = new JSONArray(js.get(DATA_BASIC[0]).toString());
-                 partnersArray = new JSONArray(js.get(DATA_BASIC[1]).toString());
-                 placesArray = new JSONArray(js.get(DATA_BASIC[2]).toString());
-                 presentationsArray = new JSONArray(js.get(DATA_BASIC[3]).toString());
-                 speakersArray = new JSONArray(js.get(DATA_BASIC[4]).toString());
-                 usersArray = new JSONArray(js.get(DATA_BASIC[5]).toString());
-            } catch (JSONException e) {
-                     try {
-                         organisersArray = new JSONArray(getData(DATA_BASIC[0]));
-                         partnersArray = new JSONArray(getData(DATA_BASIC[1]));
-                         placesArray = new JSONArray(getData(DATA_BASIC[2]));
-                         presentationsArray = new JSONArray(getData(DATA_BASIC[3]));
-                         speakersArray = new JSONArray(getData(DATA_BASIC[4]));
-                         usersArray = new JSONArray(getData(DATA_BASIC[5]));
-                     } catch (JSONException e1) { //TODO:Dopracować co zrobić w razie braku połączenia
-
-                        splashMessege.setText("Brak połączenia z internetem/nBłąd serwera");
-                         splashMessege.invalidate();
-                         try {
-                             this.wait(SPLASH_DISPLAY_LENGTH);
-                         } catch (InterruptedException e2) {
-                             e2.printStackTrace();
-                         }
-
-                     }
-
-            }finally {
+            if(connectionFailed)
+            {
+                    splashMessege.setText(R.string.splash_internet_error);
+                    splashMessege.invalidate();
+            }
+            else {
                 SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
                 sharedPreferences.edit().putString(getString(R.string.shared_preferences_organisers), organisersArray.toString());
                 sharedPreferences.edit().putString(getString(R.string.shared_preferences_partners), partnersArray.toString());
@@ -104,28 +100,28 @@ public class SplashScren extends Activity {
                 sharedPreferences.edit().putString(getString(R.string.shared_preferences_speakers), speakersArray.toString());
                 sharedPreferences.edit().putString(getString(R.string.shared_preferences_users), usersArray.toString());
 
-                sharedPreferences.edit().commit();
+                sharedPreferences.edit().apply();
+                startMain();
             }
-            startMain();
+            }
         }
 
-        private String getData(String urlAppend) {
+        private String getData(String urlAppend) throws JSONException {
             OkHttpClient client = new OkHttpClient();
-
+            String str = new String();
             Request request = new Request.Builder()
                     .url(DATA_HOST_URL + urlAppend)
                     .build();
             try {
                 Response res = client.newCall(request).execute();
-                String str = res.body().string();
+                str = res.body().string();
                 return str;
             } catch (IOException e) {
                 e.printStackTrace();
+                throw new JSONException("Brak polączenia");
             }
-
-            return null;
         }
 
 
     }
-}
+
