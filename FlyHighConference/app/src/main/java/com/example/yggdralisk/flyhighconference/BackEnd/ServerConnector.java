3944e3ed05c2697ccb3fbb42f7bf6fc2becb737e
LBurcon <kaix96@gmail.com>
2016-04-14 16:58:03 +0200

    Nothing

--- FlyHighConference/app/src/main/java/com/example/yggdralisk/flyhighconference/BackEnd/ServerConnector.java
+++ FlyHighConference/app/src/main/java/com/example/yggdralisk/flyhighconference/BackEnd/ServerConnector.java
@@ -322,7 +322,6 @@ public class ServerConnector {
                     }
                 });
 
-            } else {
             }
         } catch (Exception e) {
             e.printStackTrace();
